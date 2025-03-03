0ec03f462198e9aa46cf36b64a857dbfb0d52be7
seeded-enterprise-admin <seeded-enterprise-admin@example.com>
2025-03-03 14:23:05 +0530

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight-local.com:8443/api/v1/projects/cHJqOjUz/spectral.js?branch_slug=main&token=19d7ebf3-3f52-4de3-8b4e-8202e9b4e828";
+export default { extends: ruleset };
